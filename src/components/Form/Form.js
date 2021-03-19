ae05dd58ba54ae480efaef83a46a3e897dddc469
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-19 06:36:32 +0100

    working on form where users can add their memories

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,8 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
+
 import useStyles from './styles';
+
 const Form = () => {
+  const [postData, setPostData] = useState({
+    creator: '',
+    title: '',
+    message: '',
+    tags: '',
+    selectedFile: '',
+  });
   const classes = useStyles();
-  return <h1>Form</h1>;
+
+  const handleSubmit = () => {};
+
+  return (
+    <Paper className={classes.paper}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={classes.form}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">Creating a Memory</Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          value={postData.creator}
+          onChange={(e) =>
+            setPostData({ ...postData, creator: e.target.value })
+          }
+        />
+        <TextField
+          name="title"
+          variant="outlined"
+          label="Title"
+          fullWidth
+          value={postData.creator}
+          onChange={(e) =>
+            setPostData({ ...postData, creator: e.target.value })
+          }
+        />
+        <TextField
+          name="message"
+          variant="outlined"
+          label="Tags"
+          fullWidth
+          value={postData.creator}
+          onChange={(e) =>
+            setPostData({ ...postData, creator: e.target.value })
+          }
+        />
+        <TextField
+          name="tags"
+          variant="outlined"
+          label="Tags"
+          fullWidth
+          value={postData.creator}
+          onChange={(e) =>
+            setPostData({ ...postData, creator: e.target.value })
+          }
+        />
+      </form>
+    </Paper>
+  );
 };
 
 export default Form;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,7 @@
 import { combineReducers } from 'redux';
+
+import posts from './posts';
+
+export default combineReducers({
+  posts,
+});

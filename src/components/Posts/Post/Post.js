fb27094f5924562355111044ee5319133b6a7b6a
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-20 14:57:37 +0100

    make the app responsive for mobiles devices!

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -52,7 +52,7 @@ const Post = ({ post, setCurrentId }) => {
         {post.title}
       </Typography>
       <CardContent>
-        <Typography variant="h5" gutterBottom>
+        <Typography variant="body2" color="textSecondary" component="p">
           {post.message}
         </Typography>
       </CardContent>
@@ -63,7 +63,8 @@ const Post = ({ post, setCurrentId }) => {
           onClick={() => dispatch(likePost(post._id))}
         >
           <ThumbUpAltIcom fontSize="small" />
-          Like {post.likeCount}
+          &nbsp; Like &nbsp;
+          {post.likeCount}
         </Button>
         <Button
           size="small"

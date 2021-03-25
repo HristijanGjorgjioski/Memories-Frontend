7d572ae9047153a44ec015c8f8bc546fbf87cc69
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-25 06:55:45 +0100

    Forms is done, now is time to add functinallity. I want to add Google OAuth with help of react-google-auth, lets try!

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Avatar,
   Button,
@@ -9,20 +9,30 @@ import {
   TextField,
 } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Input from './Input';
 
 import useStyles from './styles';
 
 const Auth = () => {
   const classes = useStyles();
+  const [showPassword, setShowPassword] = useState(false);
 
-  const isSignup = false;
+  const [isSignup, setIsSignup] = useState(false);
+
+  const handleShowPassword = () =>
+    setShowPassword((prevShowPassword) => !prevShowPassword);
 
   const handleSubmit = () => {};
 
   const handleChange = () => {};
 
+  const switchMode = () => {
+    setIsSignup((prevIsSignup) => !prevIsSignup);
+    handleShowPassword(false);
+  };
+
   return (
-    <Container component="main" maxwidth="xs">
+    <Container component="main" maxWidth="xs">
       <Paper className={classes.paper} elevation={3}>
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
@@ -32,24 +42,60 @@ const Auth = () => {
           <Grid container spacing={2}>
             {isSignup && (
               <>
-                <Grid xs={6} mcd={12}>
-                  <TextField
-                    name="firstName"
-                    label="First Name"
-                    handleChange={handleChange}
-                    autoFocus
-                    xs={6}
-                  />
-                </Grid>
-                <TextField
+                <Input
+                  name="firstName"
+                  label="First Name"
+                  handleChange={handleChange}
+                  autoFocus
+                  half
+                />
+                <Input
                   name="lastName"
                   label="Last Name"
                   handleChange={handleChange}
-                  autoFocus
-                  xs={6}
+                  half
                 />
               </>
             )}
+            <Input
+              name="email"
+              label="Email Adress"
+              handleChange={handleChange}
+              type="email"
+            />
+            <Input
+              name="password"
+              label="Password"
+              handleChange={handleChange}
+              type={showPassword ? 'text' : 'password'}
+              handleShowPassword={handleShowPassword}
+            />
+            {isSignup && (
+              <Input
+                name="confirmPassword"
+                label="Repeat Password"
+                handleChange={handleChange}
+                type="password"
+              />
+            )}
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            {isSignup ? 'Sign Up' : 'Sign In'}
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Button onClick={switchMode}>
+                {isSignup
+                  ? 'Already have an account? Sign In'
+                  : 'Do not have an account? Sign up'}
+              </Button>
+            </Grid>
           </Grid>
         </form>
       </Paper>
